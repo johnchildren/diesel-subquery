5fe7809cd1fc06317fa66a3f422e0596f4ba8f4c
John Children <john.a.children@gmail.com>
2022-08-10 17:33:32 +0100

    Finish up basic proof of concept

--- src/lib.rs
+++ src/lib.rs
@@ -111,9 +111,7 @@ impl<S2, Query> AsSubquery<S2, Query> for Query {
 
 #[cfg(test)]
 mod tests {
-    use diesel::{debug_query, dsl::Select, pg::Pg, ExpressionMethods, QueryDsl};
-
-    use crate::SubqueryProjection;
+    use diesel::{debug_query, pg::Pg, ExpressionMethods, QueryDsl, sql_types::Text};
 
     use super::*;
 
@@ -125,70 +123,78 @@ mod tests {
         }
     }
 
-    pub mod bar {
-        use diesel::backend::Backend;
-        use diesel::dsl::Select;
-        use diesel::expression::is_aggregate::No;
-        use diesel::expression::ValidGrouping;
-        use diesel::query_builder::QueryFragment;
-        use diesel::sql_types::Int8;
-        use diesel::{AppearsOnTable, Column, Expression, SelectableExpression};
+    macro_rules! subquery {
+        ($name:ident, $query:ty, $(($col:ident, $type:ty)),+) => {
+            mod $name {
+                use diesel::backend::Backend;
+                use diesel::dsl::Select;
+                use diesel::expression::is_aggregate::No;
+                use diesel::expression::ValidGrouping;
+                use diesel::query_builder::QueryFragment;
+                use diesel::sql_types::{Int8, Text};
+                use diesel::{AppearsOnTable, Column, Expression, SelectableExpression};
 
-        use crate::Subquery;
+                use super::foo;
+                use crate::{Subquery, SubqueryProjection};
 
-        use super::foo;
-        use super::Bar;
+                #[derive(Clone)]
+                pub struct table {}
 
-        pub struct id;
+                impl SubqueryProjection<$query> for table {
+                    const RELATION_NAME: &'static str = stringify!($name);
 
-        impl Expression for id {
-            type SqlType = Int8;
-        }
+                    type PrimaryKey = id;
+                    type DefaultSelection = ($($col),+);
 
-        impl Column for id {
-            const NAME: &'static str = "id";
+                    fn primary_key() -> Self::PrimaryKey {
+                        id
+                    }
+                    fn default_selection() -> Self::DefaultSelection {
+                        ($($col),+)
+                    }
+                }
 
-            type Table = Subquery<Bar, Select<foo::table, (foo::id, foo::name)>>;
-        }
+                $(
+                pub struct $col;
 
-        impl AppearsOnTable<Subquery<Bar, Select<foo::table, (foo::id, foo::name)>>> for id {}
-        impl SelectableExpression<Subquery<Bar, Select<foo::table, (foo::id, foo::name)>>> for id {}
-        impl ValidGrouping<()> for id {
-            type IsAggregate = No;
-        }
+                impl Expression for $col {
+                    type SqlType = $type;
+                }
 
-        impl<DB: Backend> QueryFragment<DB> for id {
-            fn walk_ast<'b>(
-                &'b self,
-                mut pass: diesel::query_builder::AstPass<'_, 'b, DB>,
-            ) -> diesel::QueryResult<()> {
-                pass.push_identifier("id")
+                impl Column for $col {
+                    const NAME: &'static str = stringify!($col);
+
+                    type Table = Subquery<table, $query>;
+                }
+
+                impl AppearsOnTable<Subquery<table, $query>> for $col {}
+                impl SelectableExpression<Subquery<table, $query>> for $col {}
+                impl ValidGrouping<()> for $col {
+                    type IsAggregate = No;
+                }
+
+                impl<DB: Backend> QueryFragment<DB> for $col {
+                    fn walk_ast<'b>(
+                        &'b self,
+                        mut pass: diesel::query_builder::AstPass<'_, 'b, DB>,
+                    ) -> diesel::QueryResult<()> {
+                        pass.push_identifier(stringify!($name))?;
+                        pass.push_sql(".");
+                        pass.push_identifier(<$col as Column>::NAME)
+                    }
+                }
+                )+
             }
-        }
+        };
     }
 
-    #[derive(Clone)]
-    pub struct Bar {}
-
-    impl SubqueryProjection<Select<foo::table, (foo::id, foo::name)>> for Bar {
-        const RELATION_NAME: &'static str = "bar";
-
-        type PrimaryKey = bar::id;
-        type DefaultSelection = (bar::id,);
-
-        fn primary_key() -> Self::PrimaryKey {
-            bar::id
-        }
-        fn default_selection() -> Self::DefaultSelection {
-            (bar::id,)
-        }
-    }
+    subquery!(bar, Select<foo::table, (foo::id, foo::name)>, (id, Int8), (name, Text));
 
     #[test]
     fn it_works() {
         let sub_query = foo::table.select((foo::id, foo::name));
         //.filter(foo::age.eq(24));
-        let sub_query: Subquery<Bar, _> = sub_query.as_subquery();
+        let sub_query: Subquery<bar::table, _> = sub_query.as_subquery();
         let sub_query_string = debug_query::<Pg, _>(&sub_query).to_string();
 
         assert_eq!(
@@ -199,11 +205,11 @@ mod tests {
         let query = sub_query.clone().select((bar::id,));
         let query_string = debug_query::<Pg, _>(&query).to_string();
 
-        assert_eq!(query_string, "SELECT \"id\" FROM (SELECT \"foo\".\"id\", \"foo\".\"name\" FROM \"foo\") AS \"bar\" -- binds: []");
+        assert_eq!(query_string, "SELECT \"bar\".\"id\" FROM (SELECT \"foo\".\"id\", \"foo\".\"name\" FROM \"foo\") AS \"bar\" -- binds: []");
 
         let query = sub_query.filter(bar::id.eq(24));
         let query_string = debug_query::<Pg, _>(&query).to_string();
 
-        assert_eq!(query_string, "SELECT \"id\" FROM (SELECT \"foo\".\"id\", \"foo\".\"name\" FROM \"foo\") AS \"bar\" WHERE (\"id\" = $1) -- binds: [24]");
+        assert_eq!(query_string, "SELECT \"bar\".\"id\", \"bar\".\"name\" FROM (SELECT \"foo\".\"id\", \"foo\".\"name\" FROM \"foo\") AS \"bar\" WHERE (\"bar\".\"id\" = $1) -- binds: [24]");
     }
 }
